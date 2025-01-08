748f9778dc55e7f00c16d9bb17fd4fbfc38530a7
Mohamed99-Ahmed <mohamed202029999@gmail.com>
2025-01-08 10:24:25 +0200

    changes to deploy

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/portfolio',  // لتحديد المسار الفرعي
-  assetPrefix: '/portfolio/', // لتحديد المسار للصور والمحتويات
-  trailingSlash: true, // يضمن أن الروابط تنتهي بـ /
-  
+
 };
 
 export default nextConfig;
