c6e20b0e1e806d00c9cd4606253978b0395f479d
Mohamed99-Ahmed <mohamed202029999@gmail.com>
2025-01-08 10:32:50 +0200

    change in smll media

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Preconnect to Google's font servers for better performance */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
       </head>
