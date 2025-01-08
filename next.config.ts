7b290d41f6dc593d0f3fbafb2181ab4bc175c0dd
Mohamed99-Ahmed <mohamed202029999@gmail.com>
2025-01-08 10:09:02 +0200

    some changes in files

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  base: '/portfolio/',
+  basePath: '/portfolio',  // لتحديد المسار الفرعي
+  assetPrefix: '/portfolio/', // لتحديد المسار للصور والمحتويات
+  trailingSlash: true, // يضمن أن الروابط تنتهي بـ /
   
 };
 
